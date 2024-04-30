cb143fba05c0e4637a088ab144270734b88d71c2
unknown <jsanchezr6@est.ups.edu.ec>
2024-04-30 08:13:45 -0500

    Servicios Creados con errores2

--- src/app/pages/acercade/acercade.component.ts
+++ src/app/pages/acercade/acercade.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ContactosService } from '../../services/contactos.service';
 
 @Component({
   selector: 'app-acercade',
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './acercade.component.scss'
 })
 export class AcercadeComponent {
+
+  messages: string[] = ['Hola','Buenos dias']
+
+  constructor(private contactosService: ContactosService){
+
+
+  }
+  ngOnInit(){
+    this.messages = this.contactosService.get();
+  }
+
 
 }
