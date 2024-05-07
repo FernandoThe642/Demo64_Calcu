a15a2dc1d47c04fe3a30ad80896ed64834c6e1c9
unknown <jsanchezr6@est.ups.edu.ec>
2024-05-07 08:21:47 -0500

    Ingreso de Post

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,10 @@ import { SumaComponent } from './pages/suma/suma.component';
 import { RestaComponent } from './pages/resta/resta.component';
 import { AcercadeComponent } from './pages/acercade/acercade.component';
 import { CalculadoraComponent } from './pages/calculadora/calculadora.component';
+import { PostsComponent } from './pages/posts/posts.component';
 
 export const routes: Routes = [
+    {path: 'post', title: 'Post', component: PostsComponent},
     {path: 'suma', title: 'Operación suma', component: SumaComponent},
     {path: 'resta', title: 'Operación resta', component: RestaComponent},
     {path: 'acercade', title: 'Acerca de...', component: AcercadeComponent},
